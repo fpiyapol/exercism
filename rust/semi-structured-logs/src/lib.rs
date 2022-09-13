8ce95a6055f6c2d099c251e953d58bfe4160fbea
Piyapol Permpongpaiboon <piyapol.pppb@gmail.com>
2022-09-14 01:20:13 +0700

    learn(rust): enums and matching

---
+++ rust/semi-structured-logs/src/lib.rs
@@ -0,0 +1,29 @@
+// This stub file contains items which aren't used yet; feel free to remove this module attribute
+// to enable stricter warnings.
+#![allow(unused)]
+
+/// various log levels
+#[derive(Clone, Eq, PartialEq, Debug)]
+pub enum LogLevel {
+    Info,
+    Warning,
+    Error,
+    Debug,
+}
+
+/// primary function for emitting logs
+pub fn log(level: LogLevel, message: &str) -> String {
+    format!("[{}]: {}", format!("{:?}", level).to_uppercase(), message)
+}
+
+pub fn info(message: &str) -> String {
+    log(LogLevel::Info, message)
+}
+
+pub fn warn(message: &str) -> String {
+    log(LogLevel::Warning, message)
+}
+
+pub fn error(message: &str) -> String {
+    log(LogLevel::Error, message)
+}
